daeb46d7344f5463cbf07a68909f61097e273a4f
mgalvaro <alvaromejia.ds@gmail.com>
2025-02-20 10:08:50 +0100

    Documentación, estructura App y pruebas Alvaro

---
+++ StreamlitApp/config.py
@@ -0,0 +1,4 @@
+PAGE_CONFIG = {"page_title"            : "Datos REE",
+               "page_icon"             : ":zap:",
+               "layout"                : "wide",
+               "initial_sidebar_state" : "collapsed"}
