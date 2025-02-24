21031fe3fb51643dde12ae04e459976f89377515
javcorrfer <javiercorralesfernandez@gmail.com>
2025-02-24 21:40:28 +0100

    prueba conexion supabase2

--- StreamlitApp/main.py
+++ StreamlitApp/main.py
@@ -25,16 +25,18 @@ def run_query():
     demanda = supabase.table("demanda").select("*").eq("titulo", "Demanda").execute()
     generacion = supabase.table("generacion").select("*").execute()
     intercambios = supabase.table("intercambios").select("*").execute()
+    balance = supabase.table("balance").select("*").execute()
     
     # Accede a los datos en el atributo `.data`
     demanda_data = demanda.data if demanda and demanda.data else None
     generacion_data = generacion.data if generacion and generacion.data else None
     intercambios_data = intercambios.data if intercambios and intercambios.data else None
+    balance_data = balance.data if balance and balance.data else None
 
-    if demanda_data and generacion_data and intercambios_data:
-        return demanda_data, generacion_data, intercambios_data
+    if demanda_data and generacion_data and intercambios_data and balance_data:
+        return demanda_data, generacion_data, intercambios_data, balance_data
     else:
-        return None, None, None
+        return None, None, None, None
 
 
 def main():
@@ -48,7 +50,13 @@ def main():
     df_generacion = pd.read_csv('data/processed/DF_GENERACION_10_25_LIMPIO.csv')
     df_intercambios = pd.read_csv('data/processed/DF_INTERCAMBIOS_10_25_LIMPIO.csv')'''
 
-    df_demanda, df_generacion, df_intercambios = run_query()
+    df_demanda, df_generacion, df_intercambios, df_balance = run_query()
+
+    st.write(len(df_demanda))
+    st.write(len(df_generacion))
+    st.write(len(df_intercambios))
+    st.write(len(df_balance))
+
 
     menu = ['Inicio', 'Serie Temporal Demanda', 'Mapa de Intercambios', 'Comparación Anual', 'Generación por tecnología']
 
@@ -56,7 +64,7 @@ def main():
 
     if choice == 'Inicio':
         st.markdown('### :zap: Bienvenido a la App de datos de la REE :zap:')
-        if not df_demanda.empty and not df_generacion.empty and not df_intercambios.empty:
+        if not df_demanda.empty and not df_generacion.empty and not df_intercambios.empty and not df_balance.empy:
             st.success(":heavy_check_mark: Datos cargados con éxito")
         else:
             st.error(":exclamation: Error al cargar los datos")
