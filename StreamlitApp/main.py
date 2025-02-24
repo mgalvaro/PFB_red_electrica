371504db29978c63436b0dd16869a5faa16d1447
javcorrfer <javiercorralesfernandez@gmail.com>
2025-02-24 21:29:21 +0100

    prueba conexion supabase1

--- StreamlitApp/main.py
+++ StreamlitApp/main.py
@@ -11,16 +11,44 @@ from vis_generacion import vis_generacion
 
 st.set_page_config(**PAGE_CONFIG)
 
+def init_connection():
+    from supabase import create_client
+    url = st.secrets["SUPABASE_URL"]
+    key = st.secrets["SUPABASE_KEY"]
+    return create_client(url, key)
+
+@st.cache_data
+def run_query():
+    supabase = init_connection()
+    
+    # Ejecuta las consultas
+    demanda = supabase.table("demanda").select("*").eq("titulo", "Demanda").execute()
+    generacion = supabase.table("generacion").select("*").execute()
+    intercambios = supabase.table("intercambios").select("*").execute()
+    
+    # Accede a los datos en el atributo `.data`
+    demanda_data = demanda.data if demanda and demanda.data else None
+    generacion_data = generacion.data if generacion and generacion.data else None
+    intercambios_data = intercambios.data if intercambios and intercambios.data else None
+
+    if demanda_data and generacion_data and intercambios_data:
+        return demanda_data, generacion_data, intercambios_data
+    else:
+        return None, None, None
+
+
 def main():
 
     # st.write("Directorio actual:", os.getcwd())
     # st.write("Archivos en el directorio actual:", os.listdir("."))
     # st.write("Archivos en '../data/processed':", os.listdir("../data/processed") if os.path.exists("../data/processed") else "No encontrado")
     
-    df_demanda = pd.read_csv('data/processed/DF_DEMANDA_10_25_LIMPIO.csv')
+    '''df_demanda = pd.read_csv('data/processed/DF_DEMANDA_10_25_LIMPIO.csv')
     df_demanda = df_demanda[df_demanda['titulo'] == 'Demanda']
     df_generacion = pd.read_csv('data/processed/DF_GENERACION_10_25_LIMPIO.csv')
-    df_intercambios = pd.read_csv('data/processed/DF_INTERCAMBIOS_10_25_LIMPIO.csv')
+    df_intercambios = pd.read_csv('data/processed/DF_INTERCAMBIOS_10_25_LIMPIO.csv')'''
+
+    df_demanda, df_generacion, df_intercambios = run_query()
 
     menu = ['Inicio', 'Serie Temporal Demanda', 'Mapa de Intercambios', 'Comparación Anual', 'Generación por tecnología']
 
@@ -32,7 +60,6 @@ def main():
             st.success(":heavy_check_mark: Datos cargados con éxito")
         else:
             st.error(":exclamation: Error al cargar los datos")
-
 
     elif choice == 'Serie Temporal Demanda':
         vis_demanda(df_demanda)
@@ -48,8 +75,6 @@ def main():
     
     else:
         pass
-
-
 
 if __name__ == '__main__':
     main()
