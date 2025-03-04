c0829a8fe38ff515e53ac166d349467b38f82bfe
javcorrfer <javiercorralesfernandez@gmail.com>
2025-03-04 19:51:27 +0100

    ajuste tabs en streamlit main.py

--- StreamlitApp/main.py
+++ StreamlitApp/main.py
@@ -12,46 +12,44 @@ from functions.carga_dataframes import *
 
 from passwords import pw
 
-# Configuración de conexión
-host = "localhost"
-user = "root"
-password = pw
-database = "red_electrica"
-
 def main():
+    st.set_page_config(page_title="APP de datos REE", page_icon=None, layout="centered", initial_sidebar_state="auto", menu_items=None)
+    tab1, tab2, tab3 = st.tabs(["INICIO", "EDA", "ML"])
 
-    try:
-        df_balance, df_demanda, df_generacion, df_intercambios = carga_dataframes(host, user, password, database)
-    except Exception:
-        st.error(":exclamation: Error al cargar los datos")
-    
-    df_demanda = df_demanda[df_demanda['titulo'] == 'Demanda']
-    
-    menu = ['Inicio', 'Serie Temporal Demanda', 'Mapa de Intercambios', 'Comparación Anual', 'Generación por tecnología']
-
-    choice = st.sidebar.selectbox(label='Menu', options=menu, index=0)
-
-    if choice == 'Inicio':
+    with tab1:
         st.markdown('### :zap: Bienvenido a la App de datos de la REE :zap:')
-        if not df_demanda.empty and not df_generacion.empty and not df_intercambios.empty:
-            st.success(":heavy_check_mark: Datos cargados con éxito")
-        else:
+            
+    with tab2:
+
+        try:
+            df_balance, df_demanda, df_generacion, df_intercambios = carga_dataframes(pw["host"], pw["user"], pw["password"], pw["database"])
+            df_demanda = df_demanda[df_demanda['titulo'] == 'Demanda']
+            if not df_demanda.empty and not df_generacion.empty and not df_intercambios.empty:
+                    st.success(":heavy_check_mark: Datos cargados con éxito")
+        except Exception:
             st.error(":exclamation: Error al cargar los datos")
-
-    elif choice == 'Serie Temporal Demanda':
-        vis_demanda(df_demanda)
-
-    elif choice == 'Mapa de Intercambios':
-        vis_intercambios(df_intercambios)
-
-    elif choice == 'Comparación Anual':
-        vis_compare(df_demanda, df_generacion, df_intercambios)
-
-    elif choice == 'Generación por tecnología':
-        vis_generacion(df_generacion)
     
-    else:
-        pass
+        menu = ['Serie Temporal Demanda', 'Mapa de Intercambios', 'Comparación Anual', 'Generación por tecnología']
 
+        choice = st.selectbox(label='Menu', options=menu, index=0)
+
+        if choice == 'Serie Temporal Demanda':
+            vis_demanda(df_demanda)
+
+        elif choice == 'Mapa de Intercambios':
+            vis_intercambios(df_intercambios)
+
+        elif choice == 'Comparación Anual':
+            vis_compare(df_demanda, df_generacion, df_intercambios)
+
+        elif choice == 'Generación por tecnología':
+            vis_generacion(df_generacion)
+        
+        else:
+            pass
+    
+    with tab3:
+        st.title("EN CONSTRUCCIÓN...")
+        
 if __name__ == '__main__':
     main()
